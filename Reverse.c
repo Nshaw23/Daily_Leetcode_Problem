1c609d06570c8255089ebec4e98053cba8c5a8d9
Nitish <nitishshaw254@gmail.com>
2025-06-18 13:47:07 +0530

    Daily Leetcode POTD

---
+++ Reverse.c
@@ -0,0 +1,17 @@
+int reverse(int x){
+
+    int rem;
+    long rev = 0;
+
+    while(x!=0){
+        rem = x % 10;
+        rev = rev * 10 + rem;
+        x/=10;
+    }
+
+    if (rev > INT_MAX || rev < INT_MIN) {
+            return 0;
+    }
+
+    return (int)rev;
+}
